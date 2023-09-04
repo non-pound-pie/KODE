ec518326b5745383e9d45ada9a10718d54366e10
non-pound-pie <nurovan1@gmail.com>
2023-09-04 22:51:58 +0200

    meow

--- test_gui.py
+++ test_gui.py
@@ -8,21 +8,21 @@ root = tk.Tk()
 glob_lable = tk.Label(root, text=None)
 
 root.title('KODE')
-root.geometry('1000x700+500+200')
+root.geometry('1000x500+500+200')
 root.minsize(300, 400)
 
-root.columnconfigure(0, minsize=100)
-root.columnconfigure(1, minsize=10)
-root.columnconfigure(2, minsize=10)
+root.columnconfigure(0, minsize=5)
+root.columnconfigure(1, minsize=30)
+root.columnconfigure(2, minsize=30)
 root.columnconfigure(3, minsize=10)
-root.columnconfigure(4, minsize=300)
+root.columnconfigure(4, minsize=10)
 root.columnconfigure(5, minsize=10)
 root.columnconfigure(6, minsize=10)
 
 lable = tk.Label(root, text='Тестовое задание', 
                  font=('Arial', 20, 'bold'),
                  padx=10
-                 ).grid(row=0, column=4)
+                 ).grid(row=0, column=3)
 
 
 def getFileName():
@@ -39,9 +39,9 @@ def printObjects(fileName):
     for object in objects:
         text = text + f"{object}\n"
     # text = [f'{object}\n'for object in objects]
-    tk.Label(root, text='Имя объекта X Y Тип обекта Время создания').grid(row=11, column=4)
+    tk.Label(root, text='Имя объекта X Y Тип обекта Время создания').grid(row=4, column=3)
     glob_lable.config(text=text)
-    glob_lable.grid(row=14, column=4, rowspan=100)
+    glob_lable.grid(row=14, column=3, rowspan=100)
     return objects
 
 objects = printObjects(fileName)
@@ -59,10 +59,10 @@ def submit():
         for object in objects:
             text = text + f"{object}\n"
         glob_lable.config(text=text)
-        glob_lable.grid(row=14, column=4, rowspan=100)
+        glob_lable.grid(row=14, column=3, rowspan=100)
     except:
-        tk.Label(root, text="Введите валидные координаты (Например 12.11)").grid(row=15, column=0)
-
+        glob_lable.config(text="Введите валидные данные (Например 12.22)")
+        glob_lable.grid(row=14, column=3, rowspan=100)
 
 def groupDist():
     global glob_lable
@@ -74,7 +74,7 @@ def groupDist():
         for val in v:
             text = text + "{}\n".format(val)
     glob_lable.config(text=text)
-    glob_lable.grid(row=14, column=4, rowspan=100)
+    glob_lable.grid(row=14, column=3, rowspan=100)
 
 
 def groupName():
@@ -87,7 +87,7 @@ def groupName():
         for val in v:
             text = text + "{}\n".format(val)
     glob_lable.config(text=text)
-    glob_lable.grid(row=14, column=4, rowspan=100)
+    glob_lable.grid(row=14, column=3)
 
 
 def groupTime():
@@ -100,7 +100,7 @@ def groupTime():
         for val in v:
             text = text + "{}\n".format(val)
     glob_lable.config(text=text)
-    glob_lable.grid(row=14, column=4, rowspan=100)
+    glob_lable.grid(row=14, column=3)
 
 
 def groupType():
@@ -113,7 +113,7 @@ def groupType():
         for val in v:
             text = text + "{}\n".format(val)
     glob_lable.config(text=text)
-    glob_lable.grid(row=14, column=4, rowspan=100)
+    glob_lable.grid(row=14, column=3)
 
 
 def saveResult():
@@ -121,51 +121,54 @@ def saveResult():
     file = askopenfilename(filetypes=[('TXT', '*.txt')], title="Выберите файл...")
     save(glob_lable.cget("text"), file)
 
-lable_add = tk.Label(root,
-                    text='Добавить объект в список').grid(row=2, column=0)
 
-lable_name = tk.Label(root, text='Имя объекта').grid(row=10, column=0, sticky='nwse')
+lable_name = tk.Label(root, text='Имя объекта').grid(row=20, column=0)
 name = tk.Entry(root)
-name.grid(row=10, column=1)
+name.grid(row=20, column=1)
 
-lable_x = tk.Label(root, text='Координата X').grid(row=11, column=0)
+lable_x = tk.Label(root, text='Координата X').grid(row=21, column=0)
 x = tk.Entry(root)
-x.grid(row=11, column=1)
+x.grid(row=21, column=1)
 
-lable_y = tk.Label(root, text='Координата Y').grid(row=12, column=0)
+lable_y = tk.Label(root, text='Координата Y').grid(row=22, column=0)
 y = tk.Entry(root)
-y.grid(row=12, column=1)
+y.grid(row=22, column=1)
 
-lable_type = tk.Label(root, text='Тип объекта').grid(row=13, column=0)
+lable_type = tk.Label(root, text='Тип объекта').grid(row=23, column=0)
 type = tk.Entry(root)
-type.grid(row=13, column=1)
+type.grid(row=23, column=1)
 
-lable_submit = tk.Button(root, text='Добавить', command=submit).grid(row=14, column=0)
+lable_submit = tk.Button(root, text='Добавить объект', command=submit).grid(row=24, column=1)
 
 btn_group_dist = tk.Button(root,
                 text='Сгруппировать по расстоянию',
-                command=groupDist
-                ).grid(row=20, column=0)
+                command=groupDist,
+                width=25
+                ).grid(row=20, column=6, sticky='e')
 
 btn_group_name = tk.Button(root,
                 text='Сгруппировать по имени',
-                command=groupName
-                ).grid(row=21, column=0)
+                command=groupName,
+                width=25
+                ).grid(row=21, column=6, sticky='e')
 
 btn_group_time = tk.Button(root,
                 text='Сгруппировать по времени',
-                command=groupTime
-                ).grid(row=22, column=0)
+                command=groupTime,
+                width=25
+                ).grid(row=22, column=6, sticky='e')
 
 btn_group_type = tk.Button(root,
-                text='Сгруппировать по типу объекта',
-                command=groupType
-                ).grid(row=23, column=0)
+                text='Сгруппировать по типу',
+                command=groupType,
+                width=25
+                ).grid(row=23, column=6, sticky='e')
 
 btn_save = tk.Button(root,
                 text='Сохранить результаты в файл',
-                command=saveResult
-                ).grid(row=24, column=0)
+                command=saveResult,
+                width=25
+                ).grid(row=24, column=6, sticky='e')
     
 
 root.mainloop()
